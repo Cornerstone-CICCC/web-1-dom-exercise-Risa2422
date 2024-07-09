3eaca4dabc251315db4f8c2c702efbb862fb01a4
Risa Yamamoto <yamari2422@gmail.com>
2024-07-09 16:25:25 -0700

    finished first exercise

--- js/script.js
+++ js/script.js
@@ -1,1 +1,6 @@
 // YOUR CODE HERE
+const btn1 = document.querySelector("#btn1");
+const output1 = document.querySelector("#output1");
+btn1.addEventListener("click", () => {
+  output1.style.color = "red";
+});
