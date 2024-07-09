4b975eda2667db7e2c94729a0737d6b4b99e6f70
Risa Yamamoto <yamari2422@gmail.com>
2024-07-09 16:32:56 -0700

    finished exercise2

--- js/script.js
+++ js/script.js
@@ -4,3 +4,11 @@ const output1 = document.querySelector("#output1");
 btn1.addEventListener("click", () => {
   output1.style.color = "red";
 });
+
+const btn2 = document.querySelector("#btn2");
+const output2 = document.querySelector("#output2");
+btn2.addEventListener("click", () => {
+  const newText = document.createElement("span");
+  newText.textContent = "Hello word";
+  output2.append(newText);
+});
